729a809669b228ed2a0f3e380408b75f3c7226e5
k-sang-soo <67569730+k-sang-soo@users.noreply.github.com>
2022-06-07 14:36:22 +0900

    FEAT: gh-page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import {
-  BrowserRouter,
-  Routes,
-  Route,
+    BrowserRouter,
+    Routes,
+    Route,
 } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
-  return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <Routes>
-            <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
-            <Route path={process.env.PUBLIC_URL + "/movie/:id"} element={<Detail />} />
-          </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/movie/:id" element={<Detail />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
